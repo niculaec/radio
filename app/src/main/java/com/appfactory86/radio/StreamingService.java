7f63cf8a4d8f9435ad9cc1bcd435e5e297e7c114
niculaec <niculaec.alexandru@gmail.com>
2022-08-02 18:06:27 +0100

    StreamingService.class
    New implements of MediaPlayer and prepare was subtitute by prepareAsync

--- app/src/main/java/com/appfactory86/radio/StreamingService.java
+++ app/src/main/java/com/appfactory86/radio/StreamingService.java
@@ -1,16 +1,67 @@
 package com.appfactory86.radio;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.IBinder;
+import android.util.Log;
 
-public class StreamingService extends Service {
-    public StreamingService() {
+import java.io.IOException;
+
+public class StreamingService extends Service implements MediaPlayer.OnPreparedListener {
+    private static final String stream = "https://edge126.rcs-rds.ro/profm/profm.mp3";
+    MediaPlayer mediaPlayer = new MediaPlayer();
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer.setOnPreparedListener(this);
+
+        mediaPlayer.setAudioAttributes(new AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .build());
+        try {
+            mediaPlayer.setDataSource(stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mediaPlayer.prepareAsync();
+
+        final String CHANNELID = "Foreground Service ID";
+        NotificationChannel channel = new NotificationChannel(
+                CHANNELID,
+                CHANNELID,
+                NotificationManager.IMPORTANCE_DEFAULT);
+
+        getSystemService(NotificationManager.class).createNotificationChannel(channel);
+        Notification.Builder notificationBuilder = new Notification.Builder(this, CHANNELID)
+                .setSmallIcon(R.drawable.ic_launcher_radio);
+
+        startForeground(101, notificationBuilder.build());
+        return super.onStartCommand(intent, flags, startId);
     }
-
+    // TODO: Implementation of Binder.
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer player) {
+        mediaPlayer.start();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.release();
+        mediaPlayer = null;
     }
 }
