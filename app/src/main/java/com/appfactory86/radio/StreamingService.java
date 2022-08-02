b900b0abce27ee701474654ceef47785a6e048be
niculaec <niculaec.alexandru@gmail.com>
2022-08-02 18:13:06 +0100

    was removed unused imports

--- app/src/main/java/com/appfactory86/radio/StreamingService.java
+++ app/src/main/java/com/appfactory86/radio/StreamingService.java
@@ -6,11 +6,9 @@ import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.media.AudioAttributes;
-import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.IBinder;
-import android.util.Log;
+
 
 import java.io.IOException;
 
