fb67e75f01e44f3679da395d79e146cfbcb79e8d
niculaec <niculaec.alexandru@gmail.com>
2022-08-04 18:35:17 +0100

    Fixed MainActivity.class

--- app/src/main/java/com/appfactory86/radio/MainActivity.java
+++ app/src/main/java/com/appfactory86/radio/MainActivity.java
@@ -2,35 +2,80 @@ package com.appfactory86.radio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
     Button play;
+    boolean mBound = false;
+    StreamingService mService;
+    static String PLAY_ACTION = "Play";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(this, StreamingService.class);
+        intent.setAction(PLAY_ACTION);
+
         startForegroundService(intent);
 
         play = findViewById(R.id.button_play);
         play.setEnabled(false);
         play.setText("Loading");
 
-//        play.setOnClickListener(v -> {
-//            if (streamingService.started) {
-//                streamingService.started = false;
-//                streamingService.mediaPlayer.pause();
-//                play.setText("Play");
-//            } else {
-//                streamingService.started = true;
-//                streamingService.mediaPlayer.start();
-//                play.setText("Pause");
-//            }
-//        });
+        play.setOnClickListener(v -> {
+            if (mService.isPlaying()) {
+                mService.pause();
+                play.setText("Play");
+            } else {
+                mService.resume();
+                play.setText("Pause");
+            }
+        });
     }
+
+    private void bindService() {
+        Intent serviceIntent = new Intent(this, StreamingService.class);
+        bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        bindService();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(serviceConnection);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            StreamingService.LocalBinder binder = (StreamingService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+        }
+    };
 }
